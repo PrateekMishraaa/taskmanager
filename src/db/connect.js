e6ff068845ec5a861d2b29cb3addec9f1611c88b
arnav mishra <pm921670@gmail.com>
2025-03-13 11:44:52 +0530

    fix

--- src/db/connect.js
+++ src/db/connect.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connect = async () => {
   try {
     console.log("Attempting to connect to database.....");
-    await mongoose.connect(process.env.MONGO_URI, {});
+    await mongoose.connect("mongodb+srv://pm921670:tGavFyJf391fTXqk@tasku.5m98f.mongodb.net/?retryWrites=true&w=majority&appName=tasku", {});
     console.log("Connected to database.....");
   } catch (error) {
     console.log("Failed to connect to database.....", error.message);
